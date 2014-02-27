a14e7ee07732fcd484139457b34a07f560d82d59
Josh Devins <josh@soundcloud.com>
2014-02-27 14:47:21 +0100

    Cleanup indexing

--- index.go
+++ index.go
@@ -5,25 +5,27 @@ type posting struct {
 	offset int
 }
 
-type postings_list []posting
+type posting_list []posting
 
-type index map[sub_fingerprint]postings_list
+type index map[sub_fingerprint]posting_list
 
+// Builds an index from a fingerprint corpus. Conserves memory by simply
+// building against pointers to fingerprints in the corpus directly.
 func buildIndex(corpus []fingerprint) index {
 	idx := make(index)
 
 	for i, fp := range corpus {
 		for offset, sfp := range fp.sfps {
+
+			// need to dereference the actual fp, can't just use `&fp`
 			posting := posting{&corpus[i], offset}
 
-			// add posting to postings list for given sub-fingerprint
-			postingsList, exists := idx[sfp]
+			// add posting to posting list for given sub-fingerprint
+			pl, exists := idx[sfp]
 			if exists {
-				idx[sfp] = append(postingsList, posting)
+				idx[sfp] = append(pl, posting) // existing posting list
 			} else {
-				postingsList = make(postings_list, 1)
-				postingsList[0] = posting
-				idx[sfp] = postingsList
+				idx[sfp] = posting_list{posting} // new posting list
 			}
 		}
 	}
--- index_test.go
+++ index_test.go
@@ -41,20 +41,20 @@ func TestBuildIndex(t *testing.T) {
 	}
 
 	// should have posting list with sound one and two
-	postingsList := idx[sub_fingerprint{0, 0, 1, 0}]
-	if expected, got := 2, len(postingsList); expected != got {
+	pl := idx[sub_fingerprint{0, 0, 1, 0}]
+	if expected, got := 2, len(pl); expected != got {
 		t.Errorf("Expected %d but got %d", expected, got)
 	}
 
-	expectedPostingsList := postings_list{
+	expectedPl := posting_list{
 		posting{&corpus[0], 1},
 		posting{&corpus[1], 1},
 	}
-	for i, _ := range postingsList {
-		expectedPosting := expectedPostingsList[i]
-		posting := postingsList[i]
+	for i, _ := range pl {
+		expectedP := expectedPl[i]
+		p := pl[i]
 
-		if expected, got := (*expectedPosting.fp).id, (*posting.fp).id; expected != got {
+		if expected, got := (*expectedP.fp).id, (*p.fp).id; expected != got {
 			t.Errorf(
 				"Expected posting at index %d did not have fingerprint with ID %s but was %s",
 				i,
@@ -63,7 +63,7 @@ func TestBuildIndex(t *testing.T) {
 			)
 		}
 
-		if expected, got := expectedPosting.offset, posting.offset; expected != got {
+		if expected, got := expectedP.offset, p.offset; expected != got {
 			t.Errorf(
 				"Expected posting at index %d did not have offset %d but was %d",
 				i,
